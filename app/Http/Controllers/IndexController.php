8a402b7de7748f13acf135073617e88171ecd2ad
Andrei Chihai <andrei.chihai07@gmail.com>
2021-02-01 19:53:04 +0200

    Laravel

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class IndexController extends Controller
 {
     //
-    public function index(){
-        $message1="Message 1";
-        $message2="Message 2";
-        return view('index')->with(['message1'=> $message1, 'message2'=> $message2]);
+    public function index ()
+    { $h1= "Hello, world!";
+       $h2 = "This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.";
+      $articles= Article::select ('id', 'title', 'description') -> get();
+        return view ('index') -> with ('h1', $h1 ) -> with ('h2', $h2 )-> with ('articles', $articles );
     }
 }
